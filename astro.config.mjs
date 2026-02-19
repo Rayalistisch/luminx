22651fd31a9d227f83947a4ce06b5c524a698ac9
Ray Huffenreuter <rhuffenreuter90@gmail.com>
2026-02-19 10:08:01 +0100

    Add structured data, sitemap, robots and online marketing service route

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://luminx.nl",
+  redirects: {
+    "/diensten/webapp-development": "/diensten/online-marketing",
+  },
   image: {
     domains: ['images.pexels.com'],
   },
